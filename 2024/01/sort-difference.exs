b0501e6fe428ac7032d309191ceaa5b592d09968
SystemD Simp <145702017+SystemDsimp@users.noreply.github.com>
2024-12-03 16:51:34 +0100

    Revert "Remove module"
    This reverts commit c181e54c16855c7ec34713f4fb7b3fa89ce01ae0.

--- 2024/01/sort-difference.exs
+++ 2024/01/sort-difference.exs
@@ -1,24 +1,27 @@
-def process_file(file_path) do
-  {list1, list2} =
-    File.stream!(file_path)
-    |> Enum.reduce({[], []}, fn line, {list1, list2} ->
-      # Split the line by whitespace
-      [first, second] = String.split(line)
-      # Prepend to the respective lists
-      {[String.to_integer(first) | list1], [String.to_integer(second) | list2]}
-    end)
-  # sort the lists
-  {Enum.sort(list1), Enum.sort(list2)}
-end
-def absolute_difference(lists) do
-    {list1, list2} = lists
-    Enum.zip(list1, list2)
-    |> Enum.map(fn {a, b} -> abs(a - b) end)
+defmodule Sorter do
+  def process_file(file_path) do
+    {list1, list2} =
+      File.stream!(file_path)
+      |> Enum.reduce({[], []}, fn line, {list1, list2} ->
+        # Split the line by whitespace
+        [first, second] = String.split(line)
+        # Prepend to the respective lists
+        {[String.to_integer(first) | list1], [String.to_integer(second) | list2]}
+      end)
+    # sort the lists
+    {Enum.sort(list1), Enum.sort(list2)}
+  end
+
+  def absolute_difference(lists) do
+      {list1, list2} = lists
+      Enum.zip(list1, list2)
+      |> Enum.map(fn {a, b} -> abs(a - b) end)
+  end
 end
 
 result =
-  process_file("input.txt")
-  |> absolute_difference()
+  Sorter.process_file("input.txt")
+  |> Sorter.absolute_difference()
   |> Enum.sum()
 
 IO.inspect(result, label: "result: ")
